5524fea212488b4bd55dee7c60e86dadc7c4a8a7
Max Ng <maxwindiff@gmail.com>
2025-05-05 22:01:44 -0700

    pkg

--- Package.swift
+++ Package.swift
@@ -5,10 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "mlx-demucs",
+    platforms: [
+        .macOS("13.3")
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.0.5"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "mlx-demucs"),
+            name: "mlx-demucs",
+            dependencies: [
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXNN", package: "mlx-swift"),
+                .product(name: "MLXRandom", package: "mlx-swift"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            path: "Sources")
     ]
 )
